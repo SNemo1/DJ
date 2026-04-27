f056621e8e3d5f8e1232380093f0aea2e1e62120
Melkiiche1 <xixixixxddjddj@gmail.com>
2026-04-27 21:57:39 +0300

    swaga

--- core/settings.py
+++ core/settings.py
@@ -117,6 +117,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 import os
 MEDIA_URL = '/media/'
